e002c9362eb283cb2ee74417fc0f700663fa67f9
GanAiXia <2830202065@qq.com>
2018-09-07 10:43:36 +0800

    新添加地址页面

--- pages/newaddr/newaddr.js
+++ pages/newaddr/newaddr.js
@@ -1,3 +1,21 @@
 Page({
-
+ data: {
+    region: [],
+    customItem: '全部',
+    shqs: '省、市、区、乡镇/街道：' 
+  },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    let shq = e.detail.value
+    if (shq[0] == "全部" || shq[1] == "全部" || shq[2] == "全部") {
+        this.setData({
+            shqs: '省、市、区、乡镇/街道：请补全信息' 
+          })
+    }else{
+        this.setData({
+            region: e.detail.value
+          })
+    }
+   
+  }
 })

df1737a5a7ff4bfa6ffdc4e205784242fb5683a2
GanAiXia <2830202065@qq.com>
2018-09-21 17:02:28 +0800

    后台获取小程序用户信息、增加收货地址

--- pages/home/home.js
+++ pages/home/home.js
@@ -34,12 +34,124 @@ Page({
         }
       },
       getUserInfo: function(e) {
-        console.log(e)
-        app.globalData.userInfo = e.detail.userInfo
-        this.setData({
-          userInfo: e.detail.userInfo,
-          hasUserInfo: true
-        })
+        var that = this
+        var openId = (wx.getStorageSync('openId'))
+        if (openId) {
+          wx.getUserInfo({
+            success: function (res) {
+              that.setData({
+                userInfo: res.userInfo,
+                hasUserInfo: true
+              })
+              console.log(that.data.userInfo)
+            },
+            fail: function () {
+              // fail
+              console.log("获取失败！")
+            },
+            complete: function () {
+              // complete
+              console.log("获取用户信息完成！")
+            }
+          })
+        } else {
+          wx.login({
+            success: function (res) {
+              console.log(res.code)
+              
+              if (res.code) {
+                wx.getUserInfo({
+                  withCredentials: true,
+                  success: function (res_user) {
+                    that.setData({
+                      userInfo: res_user.rawData,
+                      hasUserInfo: true
+                    })
+                    // console.log(res_user)
+                    wx.request({
+                     //后台接口地址
+                      url: 'https://weixin.ganxia.xyz/admin/member/memberCheck',
+                      data: {
+                        code: res.code,
+                        encryptedData: res_user.encryptedData,
+                        iv: res_user.iv
+                      },
+                      method: 'POST',
+                      header: {
+                        'content-type': 'application/json'
+                      },
+                      success: function (res) {
+                        // this.globalData.userInfo = JSON.parse(res.data);
+                        var usif= JSON.parse(res_user.rawData)
+                        that.setData({
+                          userInfo: usif,
+                          hasUserInfo: true
+                        })
+                        wx.setStorageSync('openId', res.data.openid);
+                        console.log(res)
+                        console.log(that.data.userInfo)
+                      }
+                    })   
+                  }, fail: function () {
+                    wx.showModal({
+                      title: '警告通知',
+                      content: '您点击了拒绝授权,将无法正常显示个人信息,点击确定重新获取授权。',
+                      success: function (res) {
+                        if (res.confirm) {
+                          wx.openSetting({
+                            success: (res) => {
+                              if (res.authSetting["scope.userInfo"]) {////如果用户重新同意了授权登录
+                                wx.login({
+                                  success: function (res_login) {
+                                    if (res_login.code) {
+                                      wx.getUserInfo({
+                                        withCredentials: true,
+                                        success: function (res_user) {
+                                          wx.request({
+                                           url: 'https://weixin.ganxia.xyz/admin/member/memberCheck',
+                                            data: {
+                                              code: res_login.code,
+                                              encryptedData: res_user.encryptedData,
+                                              iv: res_user.iv
+                                            },
+                                            method: 'POST',
+                                            header: {
+                                              'content-type': 'application/json'
+                                            },
+                                            success: function (res) {
+                                              that.setData({
+                                                nickName: res.data.nickName,
+                                                avatarUrl: res.data.avatarUrl,
+    
+                                              })
+                                              wx.setStorageSync('openId', res.data.openId);
+                                            }
+                                          })
+                                        }
+                                      })
+                                    }
+                                  }
+                                });
+                              }
+                            }, fail: function (res) {
+    
+                            }
+                          })
+    
+                        }
+                      }
+                    })
+                  }, complete: function (res) {
+    
+    
+                  }
+                })
+              }
+            }
+          })
+    
+        }
+        
       },
       toAddr: function () {
         wx.navigateTo({
--- pages/newaddr/newaddr.js
+++ pages/newaddr/newaddr.js
@@ -2,7 +2,56 @@ Page({
  data: {
     region: [],
     customItem: '全部',
-    shqs: '省、市、区、乡镇/街道：' 
+    shqs: '省、市、区、乡镇/街道：' ,
+    userName: '',
+    userPhone: '',
+    addrDetail: '',
+    userAddrDetail: '',
+    encryptedData: '',
+    iv: '',
+    nickName:'',
+    avatarUrl: '',
+    gender: ''
+  },
+  onLoad: function() {
+    // 查看是否授权
+    var that = this
+    var openId = (wx.getStorageSync('openId'))
+    if (openId) {
+      wx.getUserInfo({
+        success: function (res) {
+          that.setData({
+            nickName: res.userInfo.nickName,
+            avatarUrl: res.userInfo.avatarUrl,
+            gender: res.userInfo.gender
+          })
+        },
+        fail: function () {
+          // fail
+          console.log("获取失败！")
+        },
+        complete: function () {
+          // complete
+          console.log("获取用户信息完成！")
+        }
+      })
+    } else {
+
+      wx.showToast({
+        title: '尚未登录，请先登录',
+        icon: 'none',
+        duration: 1500,
+        complete: function () {
+          setTimeout(() => {
+            wx.switchTab({
+              url: '/pages/home/home'
+            })
+          }, 1500);
+        }
+      })
+     
+    }
+
   },
   bindRegionChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
@@ -17,5 +66,120 @@ Page({
           })
     }
    
-  }
+  },
+  getName: function (e) {
+    // console.log(e.detail.value)  
+    let that = this
+    that.setData({
+        userName: e.detail.value
+    })
+  },
+  getPhone:function (e) {
+    // console.log(e.detail.value)
+    let that = this
+    that.setData({
+        userPhone: e.detail.value
+    })
+  },
+  getDetail:function (e) {
+    let that = this
+    that.setData({
+        addrDetail: e.detail.value
+    })
+    
+  },
+  addrsub: function () {
+    let that = this  
+    let region = that.data.region
+    let userName = that.data.userName
+    let userPhone = that.data.userPhone
+    let addrDetail = that.data.addrDetail
+    let userAddrDetail = that.data.userAddrDetail
+
+    if (userName.length == 0) {
+        wx.showToast({
+            title: '用户名不能为空啊',
+            icon: 'none',
+            duration: 2000
+        })
+    }else if (userPhone.length == 0) {
+        wx.showToast({
+            title: '电话号码不能为空啊',
+            icon: 'none',
+            duration: 2000
+          })
+    }else if(region.length == 0){
+    wx.showToast({
+        title: '省市街道不能为空啊',
+        icon: 'none',
+        duration: 2000
+      })
+    }else if (addrDetail.length == 0) {
+        wx.showToast({
+            title: '详细地址不能为空啊',
+            icon: 'none',
+            duration: 2000
+        })
+    }else if (userPhone.length <11) {
+        wx.showToast({
+            title: '电话号码长度不对啊',
+            icon: 'none',
+            duration: 2000
+          })
+    }else if (userPhone.length == 11) {
+        var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
+        if (!myreg.test(userPhone)) {
+            wx.showToast({
+                title: '手机号好像输错了啊',
+                icon: 'none',
+                duration: 1500
+            })
+        }
+    }
+
+    if (addrDetail.length > 0) {
+        userAddrDetail = region[0] + region[1] + region[2] + addrDetail
+        console.log(userAddrDetail)
+        that.setData({
+            'userAddrDetail': userAddrDetail
+        })
+        console.log(this.data)
+    }
+    var openId = wx.getStorageSync('openId')
+    wx.request({
+      //后台接口地址
+       url: 'https://weixin.ganxia.xyz/admin/member/memberAdd',
+       data: {
+        openId: openId,
+        userName: that.data.userName,
+        userPhone: that.data.userPhone,
+        userAddrDetail: that.data.userAddrDetail,
+        nickName:that.data.nickName,
+        avatarUrl: that.data.avatarUrl,
+        gender: that.data.gender
+       },
+       method: 'POST',
+       header: {
+         'content-type': 'application/json'
+       },
+       success: function (res) {
+         if (res.data.res == 1) {
+           wx.showToast({
+             title: '添加成功',
+             icon: 'success',
+             duration: 1000,
+             success: function () {
+              setTimeout(() => {
+                wx.navigateBack({
+                  delta: 1
+              })
+              }, 1000)
+             }
+           })
+         }
+         console.log(res)
+       }
+     })
+
+  } 
 })
